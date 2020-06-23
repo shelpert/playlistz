93d8283676bb9b926235206980cd39568f6f247a
Sofia Helpert <sofia@MacBook-Pro.local>
2020-06-23 10:35:22 -0500

    Jack's edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'dart:io' show Platform;
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:random_words/random_words.dart';
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_info/device_info.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+final coll = Firestore.instance.collection('users');
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,20 +21,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PlaylistName {
-  String name;
-  TextStyle style;
-  final unsaved = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
-  final saved = TextStyle(
-      fontSize: 50, fontWeight: FontWeight.bold, color: Colors.lightGreen);
-  bool isSaved;
-
-  PlaylistName() {
-    name = 'Fire Playlist';
-    style = unsaved;
-    isSaved = false;
-  }
-}
+const unsaved = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+const saved = TextStyle(
+    fontSize: 50, fontWeight: FontWeight.bold, color: Colors.lightGreen);
 
 class Initiate extends StatefulWidget {
   @override
@@ -38,271 +31,208 @@ class Initiate extends StatefulWidget {
 }
 
 class InitiateState extends State<Initiate> {
-  String name;
-
-  Future _getId() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    try {
-      if (Platform.isIOS) {
-        return deviceInfo.iosInfo;
-      } else {
-        return deviceInfo.androidInfo; // unique ID on Android
-      }
-    } on PlatformException {
-      print('Error');
-    }
+  Stream<DocumentSnapshot> stream;
+  @override
+  void initState() {
+    super.initState();
+    _setupName().then((r) => setState(() => stream = r.snapshots())); ///
   }
 
-  Future<List<Widget>> _checkUser({deviceId}) async {
-    List<Widget> returnValue;
-    final QuerySnapshot result = await Firestore.instance
-        .collection('users')
-        .where('id', isEqualTo: deviceId)
-        .getDocuments();
-
-    final List<DocumentSnapshot> docs = result.documents;
-
-    if (docs.length == 0) {
-      returnValue = <Widget>[
-        AlertDialog(
-            title: Text('What is your name?'),
-            content: TextField(
-              controller: TextEditingController(),
-              onChanged: (text) {
-                name = text;
-              },
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Submit'),
-                onPressed: () {
-                  _newUser(deviceId);
-                },
-              )
-            ])
-      ];
-      
-    } else {
-      //return <Widget>[Text('Finished')];
+  Future<DocumentReference> _setupName() async {
+    final deviceInfo = DeviceInfoPlugin();
+    final id = kIsWeb
+        ? 'anon${Random().nextInt(10000)}'
+        : Platform.isIOS
+            ? (await deviceInfo.iosInfo).identifierForVendor
+            : Platform.isAndroid
+                ? (await deviceInfo.androidInfo).androidId
+                : 'anon${Random().nextInt(10000)}';
+    final users = await coll.where('id', isEqualTo: id).getDocuments(); ///
+    if (users.documents.isNotEmpty) {
+      return users.documents.first.reference;
     }
-    return returnValue;
+    final controller = TextEditingController();
+    final name = await showDialog<String>(
+        context: context,
+        builder: (context) => AlertDialog(
+                title: Text('What is your name?'),
+                content: TextField(
+                  controller: controller,
+                  autofocus: true,
+                  onSubmitted: (text) {
+                    Navigator.pop(context, text);
+                  },
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      Navigator.pop(context, controller.text);
+                    },
+                  )
+                ]));
+    return coll.add({'id': id, 'name': name, 'saved': []});
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _getId(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            final deviceId = snapshot.data.identifierForVendor;
-            _checkUser(deviceId: deviceId).then((value) => children = value);
-          } else {
-            children = <Widget>[
-              SizedBox(
-                child: CircularProgressIndicator(),
-                width: 20,
-                height: 20,
-              ),
-              const Padding(
-                  padding: EdgeInsets.only(top: 16), child: Text('Loading...'))
-            ];
-          }
-          return Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: children));
-        });
-  }
-
-  void _newUser(deviceId) {
-    Firestore.instance
-        .collection('users')
-        .document(deviceId.data)
-        .setData({'saved': [], 'id': deviceId, 'name': name});
-    Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => Generator(id: deviceId)));
+    if (stream == null) {
+      return loading;
+    }
+    return StreamBuilder<DocumentSnapshot>(
+        stream: stream,
+        builder: (context, snapshot) =>
+            snapshot.hasData ? Generator(snapshot: snapshot.data) : loading);
   }
 }
 
+final loading = Scaffold(
+  body: Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+        SizedBox(
+          child: CircularProgressIndicator(),
+          width: 20,
+          height: 20,
+        ),
+        const Padding(
+            padding: EdgeInsets.only(top: 16), child: Text('Loading...'))
+      ])),
+);
+
 class Generator extends StatefulWidget {
-  final id;
-  Generator({this.id});
+  final DocumentSnapshot snapshot;
+  Generator({this.snapshot});
 
   @override
-  GeneratorState createState() => GeneratorState(deviceId: this.id);
+  GeneratorState createState() => GeneratorState(); ///
+}
+
+extension on DocumentSnapshot { ///
+  TextStyle style(String name) => isSaved(name) ? saved : unsaved;
+  bool isSaved(String name) => savedList.contains(name);
+  List<String> get savedList => List.from(data['saved']);
+  Future delete(String s) => _updateSaved(FieldValue.arrayRemove([s]));
+  Future add(String s) => _updateSaved(FieldValue.arrayUnion([s]));
+  Future toggle(String s) => isSaved(s) ? delete(s) : add(s);
+  Future _updateSaved(dynamic value) => reference.updateData({'saved': value});
+  String get name => data['name'];
+  String get id => data['id'];
 }
 
 class GeneratorState extends State<Generator> {
-  PlaylistName plName = PlaylistName();
-  String deviceId;
+  String name;
 
-  GeneratorState({this.deviceId});
+  @override ///
+  void initState() {
+    super.initState();
+    updateName(false);
+  }
+
+  void updateName([bool callSetState = true]) {
+    final adjective =
+        WordAdjective.random(maxSyllables: 4, safeOnly: false).asCapitalized;
+    final noun =
+        WordNoun.random(maxSyllables: 4, safeOnly: false).asCapitalized;
+    final name = [adjective, noun].join(' ');
+    if (callSetState) { ///
+      setState(() => this.name = name);
+    } else {
+      this.name = name;
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Playlistz"),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.list, size: 30),
-                onPressed: () {
-                  _awaitSavedList(context);
-                })
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text("Playlistz"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list, size: 30),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SavedList(snapshot: widget.snapshot),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Hello ${widget.snapshot.name}!",
+                style: TextStyle(fontSize: 20, color: Colors.blue)),
+            Text('Is This Name Fire?', style: TextStyle(fontSize: 40)),
+            Container(
+              height: 300,
+              //color: Colors.orange,
+              child: Center(
+                  child: Text(name,
+                      textAlign: TextAlign.center,
+                      style: widget.snapshot.style(name))),
+            ),
+            MaterialButton(
+              onPressed: updateName,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.refresh, size: 50),
+                  Text(' New', style: TextStyle(fontSize: 30))
+                ],
+              ),
+              shape: StadiumBorder(),
+              color: Colors.orange,
+              minWidth: 40,
+              height: 60,
+            )
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Is This Name Fire?', style: TextStyle(fontSize: 40)),
-              Container(
-                height: 300,
-                //color: Colors.orange,
-                child: Center(
-                    child: Text(plName.name,
-                        textAlign: TextAlign.center, style: plName.style)),
-              ),
-              MaterialButton(
-                onPressed: _newPlaylistName,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.refresh, size: 50),
-                    Text(' ', style: TextStyle(fontSize: 30)),
-                    Text('New', style: TextStyle(fontSize: 30))
-                  ],
-                ),
-                shape: StadiumBorder(),
-                color: Colors.orange,
-                minWidth: 40,
-                height: 60,
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _savePlaylistName,
-          child: Icon(Icons.whatshot, size: 30),
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white,
-        ));
-  }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _savePlaylistName,
+        child: Icon(Icons.whatshot, size: 30),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+      ));
 
-  void _newPlaylistName() {
-    setState(() {
-      String adjective =
-          WordAdjective.random(maxSyllables: 4, safeOnly: false).asCapitalized;
-      String noun =
-          WordNoun.random(maxSyllables: 4, safeOnly: false).asCapitalized;
-      plName.name = '$adjective' + ' ' + '$noun';
-      plName.style = plName.unsaved;
-      plName.isSaved = false;
-    });
-  }
-
-  void _savePlaylistName() {
-    DocumentReference arr =
-        Firestore.instance.collection('users').document(deviceId);
-    if (plName.isSaved) {
-      arr.updateData({
-        'saved': FieldValue.arrayRemove([plName.name])
-      });
-    } else {
-      arr.updateData({
-        'saved': FieldValue.arrayUnion([plName.name])
-      });
-    }
-    setState(() {
-      if (plName.isSaved) {
-        plName.isSaved = false;
-        plName.style = plName.unsaved;
-      } else {
-        plName.isSaved = true;
-        plName.style = plName.saved;
-      }
-    });
-  }
-
-  void _awaitSavedList(BuildContext context) async {
-    final result = await Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => SavedList(id: deviceId, current: plName),
-        ));
-
-    setState(() {
-      plName = result;
-    });
-  }
+  void _savePlaylistName() => widget.snapshot.toggle(name);
 }
 
 class SavedList extends StatefulWidget {
-  final id;
-  final current;
-  SavedList({this.id, this.current});
+  final DocumentSnapshot snapshot;
+  SavedList({@required this.snapshot});
 
   @override
-  SavedListState createState() =>
-      SavedListState(id: this.id, current: this.current);
+  SavedListState createState() => SavedListState();
 }
 
 class SavedListState extends State<SavedList> {
-  List<dynamic> saved;
-  PlaylistName current;
-  String id;
-  DocumentReference arr;
-
-  SavedListState({this.id, this.current});
-
   @override
   Widget build(BuildContext context) {
+    final saved = widget.snapshot.savedList;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Fire Playlistz'),
-          leading: new IconButton(
-            icon: new Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(current),
-          ),
-        ),
-        body: StreamBuilder<DocumentSnapshot>(
-            stream:
-                Firestore.instance.collection('users').document(id).snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return LinearProgressIndicator();
-              saved = snapshot.data['saved'];
-              return ListView.builder(
-                  itemCount: saved.length,
-                  itemBuilder: (context, index) {
-                    final item = saved[index];
-                    return Dismissible(
-                        key: UniqueKey(),
-                        onDismissed: (direction) {
-                          setState(() {
-                            if ('$item' == current.name) {
-                              current.style = current.unsaved;
-                              current.isSaved = false;
-                            }
-                          });
-                          _updateSaved(item);
-                        },
-                        background: Container(color: Colors.red),
-                        child: Column(children: <Widget>[
-                          ListTile(
-                              title: Text('$item',
-                                  style: TextStyle(fontSize: 20))),
-                          Divider()
-                        ]));
-                  });
-            }));
-  }
-
-  void _updateSaved(item) async {
-    arr = Firestore.instance.collection('users').document(id);
-    await arr.updateData({
-      'saved': FieldValue.arrayRemove(['$item'])
-    });
+      appBar: AppBar(title: Text("${widget.snapshot.name}'s Fire Playlistz")),
+      body: ListView.builder(
+        itemCount: saved.length,
+        itemBuilder: (context, index) {
+          final item = saved[index];
+          return Dismissible(
+            key: UniqueKey(),
+            onDismissed: (_) => widget.snapshot.delete(item),
+            background: Container(color: Colors.red),
+            child: Column(
+              children: <Widget>[
+                ListTile(title: Text('$item', style: TextStyle(fontSize: 20))),
+                Divider(),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
