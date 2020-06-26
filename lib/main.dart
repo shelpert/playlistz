27c7f80b75a78b3d852ba86b80b65a2a67eaeb8b
Sofia Helpert <sofiahelpert@gmail.com>
2020-06-26 10:21:23 -0500

    attempt to refactor with streambuilder at top

--- lib/main.dart
+++ lib/main.dart
@@ -12,79 +12,53 @@ void main() {
 }
 
 final coll = Firestore.instance.collection('users');
+String deviceId;
+
+Future<String> getId() async {
+  final deviceInfo = DeviceInfoPlugin();
+  return kIsWeb
+      ? 'anon${Random().nextInt(10000)}'
+      : Platform.isIOS
+          ? (await deviceInfo.iosInfo).identifierForVendor
+          : Platform.isAndroid
+              ? (await deviceInfo.androidInfo).androidId
+              : 'anon${Random().nextInt(10000)}';
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Playlistz", theme: ThemeData.dark(), home: Initiate());
+    getId().then((id) => deviceId = id);
+    return StreamBuilder<QuerySnapshot>(
+        stream: coll.snapshots(),
+        builder: (context, snapshot) => snapshot.hasData
+            ? GlobalSnapshot(snapshot: snapshot, child: start)
+            : loading);
   }
 }
 
-const unsaved = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
-const saved = TextStyle(
-    fontSize: 50, fontWeight: FontWeight.bold, color: Colors.lightGreen);
-
-class Initiate extends StatefulWidget {
-  @override
-  InitiateState createState() => InitiateState();
-}
-
-class InitiateState extends State<Initiate> {
-  Stream<DocumentSnapshot> stream;
-  @override
-  void initState() {
-    super.initState();
-    _setupName().then((r) => setState(() => stream = r.snapshots())); ///
-  }
-
-  Future<DocumentReference> _setupName() async {
-    final deviceInfo = DeviceInfoPlugin();
-    final id = kIsWeb
-        ? 'anon${Random().nextInt(10000)}'
-        : Platform.isIOS
-            ? (await deviceInfo.iosInfo).identifierForVendor
-            : Platform.isAndroid
-                ? (await deviceInfo.androidInfo).androidId
-                : 'anon${Random().nextInt(10000)}';
-    final users = await coll.where('id', isEqualTo: id).getDocuments(); ///
-    if (users.documents.isNotEmpty) {
-      return users.documents.first.reference;
-    }
-    final controller = TextEditingController();
-    final name = await showDialog<String>(
-        context: context,
-        builder: (context) => AlertDialog(
-                title: Text('What is your name?'),
-                content: TextField(
-                  controller: controller,
-                  autofocus: true,
-                  onSubmitted: (text) {
-                    Navigator.pop(context, text);
-                  },
-                ),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text('Submit'),
-                    onPressed: () {
-                      Navigator.pop(context, controller.text);
-                    },
-                  )
-                ]));
-    return coll.add({'id': id, 'name': name, 'saved': []});
-  }
+class GlobalSnapshot extends InheritedWidget {
+  final AsyncSnapshot<QuerySnapshot> snapshot;
 
   @override
-  Widget build(BuildContext context) {
-    if (stream == null) {
-      return loading;
-    }
-    return StreamBuilder<DocumentSnapshot>(
-        stream: stream,
-        builder: (context, snapshot) =>
-            snapshot.hasData ? Generator(snapshot: snapshot.data) : loading);
+  bool updateShouldNotify(GlobalSnapshot oldWidget) => true;
+
+  GlobalSnapshot({Widget child, this.snapshot}) : super(child: child);
+
+  static GlobalSnapshot of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<GlobalSnapshot>();
   }
 }
+
+final start = MaterialApp(
+    title: "Playlistz",
+    theme: ThemeData.dark(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Initiate(),
+      '/home': (context) => Generator(),
+      '/saved': (context) => SavedList(),
+    });
 
 final loading = Scaffold(
   body: Center(
@@ -102,15 +76,73 @@ final loading = Scaffold(
       ])),
 );
 
-class Generator extends StatefulWidget {
-  final DocumentSnapshot snapshot;
-  Generator({this.snapshot});
+const unsaved = TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+const saved = TextStyle(
+    fontSize: 50, fontWeight: FontWeight.bold, color: Colors.lightGreen);
 
+class Initiate extends StatefulWidget {
   @override
-  GeneratorState createState() => GeneratorState(); ///
+  InitiateState createState() => InitiateState();
 }
 
-extension on DocumentSnapshot { ///
+class InitiateState extends State<Initiate> {
+  Stream<DocumentSnapshot> stream;
+  @override
+  void initState() {
+    super.initState();
+    _setupName();
+  }
+
+  void _setupName() async {
+    final users = await coll.where('id', isEqualTo: deviceId).getDocuments();
+
+    ///
+    if (users.documents.isEmpty) {
+      final controller = TextEditingController();
+      final name = await showDialog<String>(
+          context: context,
+          builder: (context) => AlertDialog(
+                  title: Text('What is your name?'),
+                  content: TextField(
+                    controller: controller,
+                    autofocus: true,
+                    onSubmitted: (text) {
+                      Navigator.pop(context, text);
+                    },
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Submit'),
+                      onPressed: () {
+                        Navigator.pop(context, controller.text);
+                      },
+                    )
+                  ]));
+      coll.add({'id': deviceId, 'name': name, 'saved': []});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/home', (Route<dynamic> route) => false);
+    return Container(
+      child: Text("Hello"),
+    );
+  }
+}
+
+class Generator extends StatefulWidget {
+  //final DocumentSnapshot snapshot;
+  //Generator({this.snapshot});
+
+  @override
+  GeneratorState createState() => GeneratorState();
+
+  ///
+}
+
+extension on DocumentSnapshot {
+  ///
   TextStyle style(String name) => isSaved(name) ? saved : unsaved;
   bool isSaved(String name) => savedList.contains(name);
   List<String> get savedList => List.from(data['saved']);
@@ -120,12 +152,15 @@ extension on DocumentSnapshot { ///
   Future _updateSaved(dynamic value) => reference.updateData({'saved': value});
   String get name => data['name'];
   String get id => data['id'];
+  List<Map> get receivedList => List.from(data['received']);
 }
 
 class GeneratorState extends State<Generator> {
   String name;
 
-  @override ///
+  @override
+
+  ///
   void initState() {
     super.initState();
     updateName(false);
@@ -137,7 +172,8 @@ class GeneratorState extends State<Generator> {
     final noun =
         WordNoun.random(maxSyllables: 4, safeOnly: false).asCapitalized;
     final name = [adjective, noun].join(' ');
-    if (callSetState) { ///
+    if (callSetState) {
+      ///
       setState(() => this.name = name);
     } else {
       this.name = name;
@@ -145,66 +181,76 @@ class GeneratorState extends State<Generator> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text("Playlistz"),
-        actions: <Widget>[
-          IconButton(
+  Widget build(BuildContext context) {
+    final DocumentSnapshot snapshot = GlobalSnapshot.of(context)
+        .snapshot
+        .data
+        .documents
+        .singleWhere((element) => element['id'] == deviceId);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Playlistz"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list, size: 30),
+              onPressed: () => Navigator.pushNamed(context, '/saved'),
+            ),
+            /*IconButton(
             icon: Icon(Icons.list, size: 30),
             onPressed: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => SavedList(snapshot: widget.snapshot),
+                builder: (context) => ReceivedList,
               ),
             ),
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Hello ${widget.snapshot.name}!",
-                style: TextStyle(fontSize: 20, color: Colors.blue)),
-            Text('Is This Name Fire?', style: TextStyle(fontSize: 40)),
-            Container(
-              height: 300,
-              //color: Colors.orange,
-              child: Center(
-                  child: Text(name,
-                      textAlign: TextAlign.center,
-                      style: widget.snapshot.style(name))),
-            ),
-            MaterialButton(
-              onPressed: updateName,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.refresh, size: 50),
-                  Text(' New', style: TextStyle(fontSize: 30))
-                ],
-              ),
-              shape: StadiumBorder(),
-              color: Colors.orange,
-              minWidth: 40,
-              height: 60,
-            )
+          )*/
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _savePlaylistName,
-        child: Icon(Icons.whatshot, size: 30),
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
-      ));
-
-  void _savePlaylistName() => widget.snapshot.toggle(name);
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Hello ${snapshot.name}!",
+                  style: TextStyle(fontSize: 20, color: Colors.blue)),
+              Text('Is This Name Fire?', style: TextStyle(fontSize: 40)),
+              Container(
+                height: 300,
+                //color: Colors.orange,
+                child: Center(
+                    child: Text(name,
+                        textAlign: TextAlign.center,
+                        style: snapshot.style(name))),
+              ),
+              MaterialButton(
+                onPressed: updateName,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.refresh, size: 50),
+                    Text(' New', style: TextStyle(fontSize: 30))
+                  ],
+                ),
+                shape: StadiumBorder(),
+                color: Colors.orange,
+                minWidth: 80,
+                height: 60,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => snapshot.toggle(name),
+          child: Icon(Icons.whatshot, size: 30),
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+        ));
+  }
 }
 
 class SavedList extends StatefulWidget {
-  final DocumentSnapshot snapshot;
-  SavedList({@required this.snapshot});
+  //final DocumentSnapshot snapshot;
+  //SavedList({@required this.snapshot});
 
   @override
   SavedListState createState() => SavedListState();
@@ -213,16 +259,21 @@ class SavedList extends StatefulWidget {
 class SavedListState extends State<SavedList> {
   @override
   Widget build(BuildContext context) {
-    final saved = widget.snapshot.savedList;
+    final DocumentSnapshot snapshot = GlobalSnapshot.of(context)
+        .snapshot
+        .data
+        .documents
+        .singleWhere((element) => element['id'] == deviceId);
+    final saved = snapshot.savedList;
     return Scaffold(
-      appBar: AppBar(title: Text("${widget.snapshot.name}'s Fire Playlistz")),
+      appBar: AppBar(title: Text("${snapshot.name}'s Fire Playlistz")),
       body: ListView.builder(
         itemCount: saved.length,
         itemBuilder: (context, index) {
           final item = saved[index];
           return Dismissible(
-            key: UniqueKey(),
-            onDismissed: (_) => widget.snapshot.delete(item),
+            key: ValueKey(item),
+            onDismissed: (_) => snapshot.delete(item),
             background: Container(color: Colors.red),
             child: Column(
               children: <Widget>[
@@ -233,6 +284,96 @@ class SavedListState extends State<SavedList> {
           );
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => newName(snapshot),
+        child: Icon(Icons.add, size: 30),
+      ),
     );
   }
+
+  void newName(snapshot) async {
+    final controller = TextEditingController();
+    final name = await showDialog<String>(
+        context: context,
+        builder: (context) => AlertDialog(
+                title: Text('Add your own playlist name'),
+                content: TextField(
+                  controller: controller,
+                  autofocus: true,
+                  onSubmitted: (text) {
+                    Navigator.pop(context, text);
+                  },
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      Navigator.pop(context, controller.text);
+                    },
+                  )
+                ]));
+    snapshot.add(name);
+  }
 }
+/*
+class ReceivedList extends StatefulWidget {
+  final DocumentSnapshot snapshot;
+  ReceivedList({@required this.snapshot});
+
+  @override
+  ReceivedListState createState() => ReceivedListState();
+}
+
+class ReceivedListState extends State<ReceivedList> {
+  @override
+  Widget build(BuildContext context) {
+    final rec = widget.snapshot.receivedList;
+    return Scaffold(
+        appBar: AppBar(title: Text("Received Names")),
+        body: ListView.builder(
+            itemCount: rec.length,
+            itemBuilder: (context, index) {
+              final item = rec[index];
+              void _addReceived() {
+                widget.snapshot.add(item['name']);
+                widget.snapshot.reference.updateData({
+                  'received': FieldValue.arrayRemove([item])
+                });
+              }
+
+              return Column(
+                children: <Widget>[
+                  Expanded(child:
+                  ListTile(
+                      key: UniqueKey(),
+                      title: Text(item['name']),
+                      subtitle: Text(item['sender']),
+                      trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            MaterialButton(
+                              onPressed: _addReceived,
+                              child: Text('Add'),
+                              shape: StadiumBorder(),
+                              color: Colors.lightGreen,
+                              minWidth: 8.0,
+                            ),
+                            SizedBox(
+                              width: 15.0,
+                            ),
+                            MaterialButton(
+                                onPressed: () =>
+                                    widget.snapshot.reference.updateData({
+                                      'received': FieldValue.arrayRemove([item])
+                                    }),
+                                child: Text('Nah'),
+                                shape: StadiumBorder(),
+                                color: Colors.red,
+                                minWidth: 8.0),
+                          ]))),
+                  Divider(),
+                ],
+              );
+            }));
+  }
+}*/
